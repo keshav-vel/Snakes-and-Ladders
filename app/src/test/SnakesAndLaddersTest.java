4d8dc6909b543bc7762343d2fe589d8be28b2126
Keshav Rathinavel <keshavr@sahaj.ai>
2024-06-17 23:24:52 +0530

    Create test that ensures successful run of the game

--- app/src/test/SnakesAndLaddersTest.java
+++ app/src/test/SnakesAndLaddersTest.java
@@ -1,16 +1,26 @@
 package app.src.test;
 
-import app.src.main.gamedie.GameDie;
+import app.src.main.SnakesAndLadders;
+import app.src.test.utils.ProgrammableDie;
+import app.src.test.utils.TestLogger;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class SnakesAndLaddersTest {
 
-    class ProgrammableDie implements GameDie {
-
-        int index = 0;
-        Integer[] moves = {5,3,2,4,2,4,5,6,4,6,3,3,1,6,5,3,3,4,3,3,1,4,5,5,3,6,2,1,5,4,2,1,4,5,1,4,2,1,6,4,3,1,1,4,5,1,4,5,4,6,6,6,4,6,1,5,2,1,6,3,1,4,6,1};
-        @Override
-        public Integer roll() {
-            return moves[index++];
+    @Test
+    void gameRunsAndEndsSuccessfully() {
+        SnakesAndLadders snakesAndLadders = new SnakesAndLadders();
+        TestLogger testLogger = new TestLogger();
+        try {
+            snakesAndLadders.run(new ProgrammableDie(), testLogger, () -> {
+                throw new RuntimeException("Game Finished");
+            });
+            fail("Exception to be thrown!");
+        } catch (RuntimeException e) {
+            assertEquals("Player four wins! Game finished.", testLogger.messageAt(testLogger.size()-1));
         }
     }
 }
