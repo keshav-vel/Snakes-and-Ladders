09f8040f55a883d4dbdb67ad7a63218a57df6d70
Keshav Rathinavel <keshavr@sahaj.ai>
2024-06-17 23:23:58 +0530

    Create ProgrammableDie containing moves for a winning scenario

---
+++ app/src/test/utils/ProgrammableDie.java
@@ -0,0 +1,13 @@
+package app.src.test.utils;
+
+import app.src.main.gamedie.GameDie;
+
+public class ProgrammableDie implements GameDie {
+    int index = 0;
+    Integer[] moves = {5, 3, 2, 4, 2, 4, 5, 6, 4, 6, 3, 3, 1, 6, 5, 3, 3, 4, 3, 3, 1, 4, 5, 5, 3, 6, 2, 1, 5, 4, 2, 1, 4, 5, 1, 4, 2, 1, 6, 4, 3, 1, 1, 4, 5, 1, 4, 5, 4, 6, 6, 6, 4, 6, 1, 5, 2, 1, 6, 3, 1, 4, 6, 1};
+
+    @Override
+    public Integer roll() {
+        return moves[index++];
+    }
+}
